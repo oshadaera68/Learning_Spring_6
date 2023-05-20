11439b07d4a5394047a1bfc661c3aa6e60990e15
oshadaera68 <mindartlk010@gmail.com>
2023-05-20 22:10:25 +0530

    first commit

---
+++ spring-6-demo/src/main/java/com/eraboy/spring6demo/Spring6DemoApplication.java
@@ -0,0 +1,13 @@
+package com.eraboy.spring6demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6DemoApplication.class, args);
+	}
+
+}
---
+++ spring-6-demo/src/test/java/com/eraboy/spring6demo/Spring6DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eraboy.spring6demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
